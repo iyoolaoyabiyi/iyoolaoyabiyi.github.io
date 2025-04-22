7b3ae4c8a07b4b7f44c3f64080eb7a7a40c2dadf
Iyola Oyabiyi O <iyolaoyabiyi@gmail.com>
2025-04-22 01:51:09 +0100

    Enhance GUI layout and functionality with improved menu handling and styles

--- js/gui.js
+++ js/gui.js
@@ -1,26 +1,53 @@
 const gui = {
-  isMenuOpen: false,
+  isMenuOpen: true,
   activate() {
     const icon = document.querySelector('.nav-icon');
+    const logoContainer = document.querySelector('.header-logo');
     const lines = document.querySelectorAll('.nav-icon-line');
     const nav = document.querySelector('.header-nav');
     icon.addEventListener('click', () => {
       if (!gui.isMenuOpen) {
-        lines.forEach((line, index) => {
-          line.style.transform = index % 2 ? 'rotate(45deg)' : 'rotate(-45deg)';
-        });
-        nav.style.display = 'block';
-        nav.style.visibility = 'visible';
+        openMenu();
       } else {
-        lines.forEach((line, index) => {
-          line.style.transform =   'rotate(0)';
-          line.style.transform = index % 2 ? 'translateY(-5px)' : 'translateY(5px)';
-        });
-        nav.style.display = 'none';
-        nav.style.visibility = 'hidden';
+        closeMenu();
       }
-      gui.isMenuOpen = !gui.isMenuOpen;
+      
     });
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 40) {
+        if (this.isMenuOpen) {
+          icon.parentElement.style.display = 'block';
+          logoContainer.style.margin = '0';
+          closeMenu();
+        } 
+      } else {
+          icon.parentElement.style.display = 'none';
+          logoContainer.style.margin = 'auto';
+          if (!this.isMenuOpen) {
+            openMenu();
+          }
+        }
+    });
+    function openMenu() {
+      nav.style.display = 'block';
+      setTimeout(() => {
+        nav.style.opacity = '1';
+      }, 300);
+      lines.forEach((line, index) => {
+        line.style.transform = index % 2 ? 'rotate(45deg)' : 'rotate(-45deg)';
+      });
+      gui.isMenuOpen = true;
+    }
+    function closeMenu() {
+      nav.style.opacity = '0';
+      setTimeout(() => {
+        nav.style.display = 'none';
+      }, 300);
+      lines.forEach((line, index) => {
+        line.style.transform = index % 2 ? 'translateY(-5px)' : 'translateY(5px)';
+      });
+      gui.isMenuOpen = false;
+    }
   }
 }
 
